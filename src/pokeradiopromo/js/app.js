7512502da63cdd28234aac48bba54261af7b46c4
Jamie Ingram <jamie@pokelondon.com>
2015-02-11 00:14:08 +0000

    svg tweaks

--- src/pokeradiopromo/js/app.js
+++ src/pokeradiopromo/js/app.js
@@ -64,10 +64,65 @@ require(["jquery",
                 });
 
                 var Vivus = window.Vivus;
-                var diagram = new Vivus('HowItWorks-diagram', {type: 'scenario', duration: 200});
-
+                var diagram = new Vivus('HowItWorks-diagram', {type: 'scenario-sync', duration: 20});
             });
 
+            //retrieve currently playing info
+            var name = '';
+            var artist = '';
+            var image_url = 'img/track.jpg';
+
+            $.getJSON(
+                'http://pokerad.io/api/playlist/'
+            )
+            .done(function(json) {
+                var track = json[0];
+                if (track.played == true && json.length > 1) {
+                    for (var i = 1 ; i < json.length ; i++) {
+                        track = json[i];
+                        if (track.played == false) break;
+                    }
+                }
+                name = track.name;
+                artist = track.artist;
+                var album_id = track.album_href.split(":")[2];
+                image_url = getAlbumImage(album_id);
+
+            })
+            .fail(function(jqxhr, textStatus, error) {
+                var err = textStatus + ", " + error;
+                console.log( "Request Failed: " + err );
+            });
+
+            var getAlbumImage = function (album_id) {
+                $.getJSON(
+                    'https://api.spotify.com/v1/albums/' + album_id
+                )
+                .done(function(json) {
+                    for (var i = 0 ; i < json.images.length ; i++) {
+                        var image = json.images[i];
+                        if (image.width > 140) {
+                            image_url =  image.url;
+                            break;
+                        }
+                    }
+                    populateTrack(name, artist, image_url);
+                })
+                .fail(function(jqxhr, textStatus, error) {
+                    var err = textStatus + ", " + error;
+                    console.log( "Albumn Request Failed: " + err );
+                });
+            }
+
+            var populateTrack = function (name, artist, image_url) {
+                console.log(name);
+                var image_el = $('.track-image');
+                var name_el = $('.track-title');
+                var artist_el = $('.track-artist');
+                image_el.attr("src", image_url);
+                name_el.text(name);
+                artist_el.text(artist);
+            }
         })();
         
     }
